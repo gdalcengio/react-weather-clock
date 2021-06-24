95d174f83c43468b8da819f9df8c63260b520507
Gabriele Dal Cengio <gabriele_dal_cengio@sfu.ca>
2021-06-24 14:30:56 -0700

    Set up node proxy server index.js to hide api key when fetching weather data

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.use(cors());
+
+app.get("/", function (req, res) {
+  const cityName = "Surrey"; //to be replaced with post data
+  const apiKey = process.env.API_KEY;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast/?q=${cityName}&appid=${apiKey}`;
+
+  console.log(apiKey);
+  fetch(weatherUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch(console.log);
+});
+
+app.listen(3001, function () {
+  console.log("app listening on port 3001");
+});
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,17 @@
 import React from "react";
 import "./weather.css";
 
-const api = {
-  key: "0cda82f90f0840d9bb26a75c6f1d948f",
-  base: "https://api.openweathermap.org/data/2.5/forecast",
-};
+// const getWeatherData = async () => {
+//   const cityName = "Surrey";
+//   try {
+//     const resp = await fetch(api.base + "?q=" + cityName + "&appid=" + api.key);
+//     const data = await resp.json();
 
-const getWeatherData = async () => {
-  const cityName = "Surrey";
-  try {
-    const resp = await fetch(api.base + "?q=" + cityName + "&appid=" + api.key);
-    const data = await resp.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
 function Card(props) {
   return (
@@ -29,19 +24,47 @@ function Card(props) {
   );
 }
 
-function Weather() {
-  const day = "mon";
-  const degrees = 0;
-  const status = "status";
-  const thumbnail = " ./thumbnail";
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      day: "mon",
+      degrees: 0,
+      status: "status",
+      thumbnail: "./thumbnail",
+    };
+  }
 
-  getWeatherData().then((result) => console.log(result));
+  //change to onsearchbarkeychange later
+  componentDidMount() {
+    // const cityName = "Surrey"; // not BC in GB hahaha either way its a test
+    //fetch url will become "http://localhost:3001" to avoid conflict with 3000
+    fetch("http://localhost:3001")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(console.log);
+  }
 
-  return (
-    <div className="weather-bar">
-      <Card day={day} thumbnail={thumbnail} status={status} degrees={degrees} />
-    </div>
-  );
+  //   const day = "mon";
+  //   const degrees = 0;
+  //   const status = "status";
+  //   const thumbnail = " ./thumbnail";
+
+  //   getWeatherData().then((result) => console.log(result));
+  render() {
+    return (
+      <div className="weather-bar">
+        <Card
+          day={this.state.day}
+          thumbnail={this.state.thumbnail}
+          status={this.state.status}
+          degrees={this.state.degrees}
+        />
+      </div>
+    );
+  }
 }
 
 export default Weather;
